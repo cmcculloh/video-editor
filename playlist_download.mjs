aeede26c15beffff5bab4b1e9a1c170f9198c7b7
cmcculloh <cmcculloh@gmail.com>
2023-04-24 09:15:31 -0400

    Makes it actually work

--- playlist_download.mjs
+++ playlist_download.mjs
@@ -5,6 +5,9 @@ import https from "https";
 const execPromisified = util.promisify(exec);
 const writeFile = util.promisify(fs.writeFile);
 const readFile = util.promisify(fs.readFile);
+import { pipeline } from "stream/promises";
+import stream from "stream";
+import fetch from "node-fetch";
 
 const playlistUrl = process.argv[2];
 const cookiesFilePath = "cookies.txt";
@@ -13,14 +16,13 @@ if (!fs.existsSync("downloads")) {
 	fs.mkdirSync("downloads");
 }
 
-// Generate a unique filename based on the playlist URL
 let downloadedVideosFile;
 let outputDir;
 
 if (playlistUrl.includes("playlist")) {
 	const playlistId = new URL(playlistUrl).searchParams.get("list");
 	downloadedVideosFile = `downloads/downloaded_videos_${playlistId}.json`;
-	outputDir = `downloads/${playlistId}`;
+	outputDir = `/Volumes/McCullohShare/Plex/TV/${playlistId}`;
 } else if (playlistUrl.includes("videos")) {
 	let channelName;
 
@@ -36,7 +38,7 @@ if (playlistUrl.includes("playlist")) {
 	}
 
 	downloadedVideosFile = `downloads/downloaded_videos_${channelName}.json`;
-	outputDir = `downloads/${channelName}`;
+	outputDir = `/Volumes/McCullohShare/Plex/TV/${channelName}`;
 } else {
 	console.error("Invalid URL format. Please provide a playlist or videos URL.");
 	process.exit(1);
@@ -47,22 +49,18 @@ if (!fs.existsSync(outputDir)) {
 }
 
 async function downloadVideo(url, filename) {
-	return new Promise((resolve, reject) => {
-		const file = fs.createWriteStream(filename);
-		https
-			.get(url, (response) => {
-				response.pipe(file);
-				file.on("finish", () => {
-					file.close(resolve);
-				});
-			})
-			.on("error", (error) => {
-				fs.unlink(filename);
-				reject(error);
-			});
+	return new Promise(async (resolve, reject) => {
+		try {
+			const response = await fetch(url);
+			const fileStream = fs.createWriteStream(filename);
+			await pipeline(response.body, fileStream);
+			resolve();
+		} catch (error) {
+			fs.unlink(filename, () => {});
+			reject(error);
+		}
 	});
 }
-
 async function getVideoInfo(videoId) {
 	const { stdout: jsonOutput } = await execPromisified(
 		`yt-dlp --cookies ${cookiesFilePath} -j https://www.youtube.com/watch?v=${videoId}`,
@@ -86,15 +84,30 @@ async function getVideoInfo(videoId) {
 
 		const videoIds = ids
 			.split("\n")
-			.filter((id) => id && !downloadedData.some((entry) => entry.id === id))
-			.reverse();
+			.filter((id) => id && !downloadedData.some((entry) => entry.id === id));
 
-		let seasonCounter =
-			downloadedData.length > 0 ? downloadedData[downloadedData.length - 1].season : 1;
-		let episodeCounter =
-			downloadedData.length > 0 ? downloadedData[downloadedData.length - 1].episode + 1 : 1;
+		if (playlistUrl.includes("videos")) {
+			videoIds.reverse();
+		}
+		console.log(`Getting info for videoId: ${videoIds[0]}`, videoIds.join(", "));
+		const firstVideoInfo = await getVideoInfo(videoIds[0]);
+		const firstVideoUploadDate = firstVideoInfo.upload_date;
+
+		let seasonCounter;
+		let episodeCounter;
+
+		if (downloadedData.length > 0) {
+			const lastEntry = downloadedData[downloadedData.length - 1];
+			seasonCounter = lastEntry.season;
+			episodeCounter = lastEntry.episode + 1;
+		} else {
+			const firstYear = parseInt(firstVideoUploadDate.slice(0, 4), 10);
+			seasonCounter = 1;
+			episodeCounter = 1;
+		}
 
 		for (const videoId of videoIds) {
+			console.log(`Getting info for videoId: ${videoId}`);
 			const { stdout: videoUrl } = await execPromisified(
 				`yt-dlp -i -f best --cookies ${cookiesFilePath} --get-url https://www.youtube.com/watch?v=${videoId}`,
 				{ maxBuffer: 10 * 1024 * 1024 }
@@ -102,6 +115,11 @@ async function getVideoInfo(videoId) {
 
 			const videoInfo = await getVideoInfo(videoId);
 			const title = videoInfo.title;
+			const videoUploadDate = videoInfo.upload_date;
+
+			const currentYear = parseInt(videoUploadDate.slice(0, 4), 10);
+			const firstYear = parseInt(firstVideoUploadDate.slice(0, 4), 10);
+			const seasonCounter = currentYear - firstYear + 1;
 
 			const fileName = `${outputDir}/S${seasonCounter
 				.toString()
@@ -129,3 +147,4 @@ async function getVideoInfo(videoId) {
 		console.error(`Error: ${error}`);
 	}
 })();
+
